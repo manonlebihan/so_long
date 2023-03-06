a6eaa3d18df65f03b04d277e39db271d17686231
Manon Le Bihan <manonlebihan@MBP-de-Manon.lan>
2023-03-06 17:53:56 +0100

    06032023 - 17:53

--- srcs/direction.c
+++ srcs/direction.c
@@ -6,11 +6,31 @@
 /*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 16:55:44 by mle-biha          #+#    #+#             */
-/*   Updated: 2023/03/06 16:56:14 by mle-biha         ###   ########.fr       */
+/*   Updated: 2023/03/06 17:51:10 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+void	move_up()
+{
+	
+}
+
+void	move_down()
+{
+	
+}
+
+void	move_left()
+{
+	
+}
+
+void	move_right()
+{
+	
+}
 
 int	direction(int keycode, t_solong *sl)
 {
