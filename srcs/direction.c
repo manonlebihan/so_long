7d56ba0b41a4a9c71457dd67da5110ef2894f104
manonlebihan <manonlebihan1999@gmail.com>
2023-03-07 12:33:06 +0100

    07032023 - 12:33

--- srcs/direction.c
+++ srcs/direction.c
@@ -6,7 +6,7 @@
 /*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 16:55:44 by mle-biha          #+#    #+#             */
-/*   Updated: 2023/03/07 12:11:14 by mle-biha         ###   ########.fr       */
+/*   Updated: 2023/03/07 12:32:03 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	move_up(t_solong *sl)
 {
-	sl->map.count++;
 	if (sl->map.map[sl->map.player_y - 1][sl->map.player_x] != '1' &&
 			sl->map.map[sl->map.player_y - 1][sl->map.player_x] != 'E')
 	{
+		sl->map.count++;
 		if (sl->map.map[sl->map.player_y - 1][sl->map.player_x] == 'C')
 			sl->map.collectible--;
 		sl->map.map[sl->map.player_y - 1][sl->map.player_x] = 'P';
@@ -28,6 +28,7 @@ void	move_up(t_solong *sl)
 	else if (sl->map.map[sl->map.player_y - 1][sl->map.player_x] == 'E' &&
 			sl->map.collectible == 0)
 	{
+		sl->map.count++;
 		ft_printf("You won with %d moves !\n", sl->map.count);
 		free_mlx(&(sl->mlx), &(sl->map));
 	}
@@ -36,10 +37,10 @@ void	move_up(t_solong *sl)
 
 void	move_down(t_solong *sl)
 {
-	sl->map.count++;
 	if (sl->map.map[sl->map.player_y + 1][sl->map.player_x] != '1' &&
 			sl->map.map[sl->map.player_y + 1][sl->map.player_x] != 'E')
 	{
+		sl->map.count++;
 		if (sl->map.map[sl->map.player_y + 1][sl->map.player_x] == 'C')
 			sl->map.collectible--;
 		sl->map.map[sl->map.player_y + 1][sl->map.player_x] = 'P';
@@ -50,6 +51,7 @@ void	move_down(t_solong *sl)
 	else if (sl->map.map[sl->map.player_y + 1][sl->map.player_x] == 'E' &&
 			sl->map.collectible == 0)
 	{
+		sl->map.count++;
 		ft_printf("You won with %d moves !\n", sl->map.count);
 		free_mlx(&(sl->mlx), &(sl->map));
 	}
@@ -58,10 +60,10 @@ void	move_down(t_solong *sl)
 
 void	move_left(t_solong *sl)
 {
-	sl->map.count++;
 	if (sl->map.map[sl->map.player_y][sl->map.player_x - 1] != '1' &&
 			sl->map.map[sl->map.player_y][sl->map.player_x - 1] != 'E')
 	{
+		sl->map.count++;
 		if (sl->map.map[sl->map.player_y][sl->map.player_x - 1] == 'C')
 			sl->map.collectible--;
 		sl->map.map[sl->map.player_y][sl->map.player_x - 1] = 'P';
@@ -72,6 +74,7 @@ void	move_left(t_solong *sl)
 	else if (sl->map.map[sl->map.player_y][sl->map.player_x - 1] == 'E' &&
 			sl->map.collectible == 0)
 	{
+		sl->map.count++;
 		ft_printf("You won with %d moves !\n", sl->map.count);
 		free_mlx(&(sl->mlx), &(sl->map));
 	}
@@ -80,7 +83,6 @@ void	move_left(t_solong *sl)
 
 void	move_right(t_solong *sl)
 {
-	sl->map.count++;
 	if (sl->map.map[sl->map.player_y][sl->map.player_x + 1] != '1' &&
 			sl->map.map[sl->map.player_y][sl->map.player_x + 1] != 'E')
 	{
@@ -89,11 +91,13 @@ void	move_right(t_solong *sl)
 		sl->map.map[sl->map.player_y][sl->map.player_x + 1] = 'P';
 		sl->map.map[sl->map.player_y][sl->map.player_x] = '0';
 		sl->map.player_x++;
+		sl->map.count++;
 		ft_printf("Number of moves : %d\n", sl->map.count);
 	}
 	else if (sl->map.map[sl->map.player_y][sl->map.player_x + 1] == 'E' &&
 			sl->map.collectible == 0)
 	{
+		sl->map.count++;
 		ft_printf("You won with %d moves !\n", sl->map.count);
 		free_mlx(&(sl->mlx), &(sl->map));
 	}
