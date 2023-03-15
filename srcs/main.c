ffbd72c2effac08fdb098400e8a075b6b75b9f2e
manonlebihan <manonlebihan1999@gmail.com>
2023-03-15 18:07:22 +0100

    15032023 - 18:07

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 16:23:09 by mle-biha          #+#    #+#             */
-/*   Updated: 2023/03/15 17:30:15 by mle-biha         ###   ########.fr       */
+/*   Updated: 2023/03/15 18:07:10 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ int	check_map(int argc,	char *filename, t_map *map)
 
 	if (argc != 2)
 	{
-		ft_putendl_fd("Error\nNot enough arguments : ./so_long [file.ber]", 2);
+		ft_putendl_fd("Error\nNumber of arguments invalid : ./so_long [file.ber]", 2);
 		exit(EXIT_FAILURE);
 	}
 	fd = open(filename, O_RDONLY);
